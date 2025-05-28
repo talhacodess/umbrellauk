ab984f4a1025aa1d2eb8ce337c858ff71f6c6fea
Talha Bashir <contact@talhacodes.com>
2025-05-29 01:07:36 +0500

    commit

--- umbrellauk/components/Container.jsx
+++ umbrellauk/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children}) {
   return (
-    <div className='max-w-[1400px] mx-[133px]'>{children}</div>
+    <div className='max-w-[1400px] mx-[133px] p-2'>{children}</div>
   )
 }
 
--- umbrellauk/header/Header.jsx
+++ umbrellauk/header/Header.jsx
@@ -11,33 +11,43 @@ import { FaXTwitter, FaYoutube } from 'react-icons/fa6'
 
 function Header() {
   return (
-    <div>
-        <Container>
-          <div className='flex w-full align-items-center justify-between '>
-            <div className=' w-6/12'>
+
+      <Container>
+        <div className='flex w-full  justify-between '>
+          <div className='w-full sm:w-6/12 '>
             <div className='flex gap-5'>
               <div className='flex gap-2 icon-flags'><img src={uk} alt="Uk Flag" width={32} height={15} /><span>UK</span></div>
               <div className='flex gap-2 icon-flags'><img src={us} alt="USA flag" width={32} height={15} /><span>USA</span></div>
               <div className='flex gap-2'><img src={canada} alt="Canada Flag" width={32} height={15} /><span>CA</span></div>
             </div>
+          </div>
+          <div className='w-full sm:w-6/12'>
+            <div className="flex gap-1 justify-end">
+              <div className="bg-[#f3f3f3] p-2 rounded hover:text-[#ff931e] hover:scale-105 transform transition-transform duration-200">
+                <FaInstagram />
+              </div>
+              <div className="bg-[#f3f3f3] p-2 rounded hover:text-[#ff931e] hover:scale-105 transform transition-transform duration-200">
+                <FaXTwitter />
+              </div>
+              <div className="bg-[#f3f3f3] p-2 rounded hover:text-[#ff931e] hover:scale-105 transform transition-transform duration-200">
+                <FaYoutube />
+              </div>
+              <div className="bg-[#f3f3f3] p-2 rounded hover:text-[#ff931e] hover:scale-105 transform transition-transform duration-200">
+                <FaLinkedin />
+              </div>
+              <div className="bg-[#f3f3f3] p-2 rounded hover:text-[#ff931e] hover:scale-105 transform transition-transform duration-200">
+                <FaPinterest />
+              </div>
             </div>
-            <div className=' w-6/12 '>
-            <div className='flex   gap-1 '>
-              <div className=' bg-[#f3f3f3] p-2 '><FaFacebook /></div>
-              <div className=' bg-[#f3f3f3] p-2 '><FaInstagram /></div>
-              <div className=' bg-[#f3f3f3] p-2 '><FaXTwitter /></div>
-              <div className=' bg-[#f3f3f3] p-2 '><FaYoutube /></div>
-              <div className=' bg-[#f3f3f3] p-2 '><FaLinkedin /></div>
-              <div className=' bg-[#f3f3f3] p-2 '><FaPinterest /></div>
-            </div>
-            </div>
-
 
           </div>
-            
-        </Container>
-      
-    </div>
+
+
+        </div>
+
+      </Container>
+
+  
   )
 }
 
